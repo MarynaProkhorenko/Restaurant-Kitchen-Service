ad405598608b144f4bb52c3370b2a977aebb4b81
Maryna Prokhorenko <maryna.prokhorenko11@gmail.com>
2023-02-09 10:16:25 +0200

    Add model Dish to the models in the kitchen

--- kitchen/models.py
+++ kitchen/models.py
@@ -9,3 +9,14 @@ class DishType(models.Model):
 
     def __str__(self):
         return {self.name}
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_length=15, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(Cook, related_name="dishes")
+
+    def __str__(self):
+        return f"{self.name}( {self.dish_type}) costs {self.price}"
