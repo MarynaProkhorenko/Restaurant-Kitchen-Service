bb1330325ca9408886666deb43b2094c0acc728d
Maryna Prokhorenko <maryna.prokhorenko11@gmail.com>
2023-03-14 16:08:22 +0200

    Edited secret key

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "xjw+_=av)ra&rq3l0$u@bcdokyw_&%)1feo#ps0njjncl^-b9s"
+SECRET_KEY = ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
