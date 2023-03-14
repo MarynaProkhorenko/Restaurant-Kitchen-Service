50a74984ea61fccc2cf0820250e8bf5964ac6944
Maryna Prokhorenko <maryna.prokhorenko11@gmail.com>
2023-03-14 13:52:51 +0200

    Added buttons to check ingredients and dish types to creatind dish page

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+SECRET_KEY = "xjw+_=av)ra&rq3l0$u@bcdokyw_&%)1feo#ps0njjncl^-b9s"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
